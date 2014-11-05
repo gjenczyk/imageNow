f28fcf088026407c43b47440bc73320c642cb24a
unknown <gjenczyk@SHRLLENOR9XLEP1.ad.umassp.edu>
2014-11-05 12:02:02 -0500

    Improved PDF handling

--- INMAC mods/ghost.ps1
+++ INMAC mods/ghost.ps1
@@ -18,16 +18,35 @@
 # CONFIG #
 
 $pdf = $args[0] 
-#"pdf is $pdf"| Out-File D:\INMAC\ne.txt -Append 
+#"pdf is $pdf"| Out-File  D:\INMAC\${inmacVersion}_powerghost.log -Append
 $outDir = $args[1] 
-#"outDir is $outDir"| Out-File D:\INMAC\ne.txt -Append 
+#"outDir is $outDir"| Out-File  D:\INMAC\${inmacVersion}_powerghost.log -Append
 $pdfName = $args[2] 
-#"pdfName is $pdfName"| Out-File D:\INMAC\ne.txt -Append 
+#"pdfName is $pdfName"| Out-File  D:\INMAC\${inmacVersion}_powerghost.log -Append
 $inmacVersion = $args[3]
-#"inmac version is $inmacVersion" | Out-File D:\INMAC\ne.txt -Append
+#"inmac version is $inmacVersion" | Out-File  D:\INMAC\${inmacVersion}_powerghost.log -Append
+$pdfPath = $pdf+$pdfName 
+# Tool Paths #
+$default = "D:\Program Files\gs\gs9.14\bin\gswin64c.exe"
+$repair = "D:\Program Files\gs\gs\bin\gswin32c.exe" 
+#"pdfPath is $pdfPath"| Out-File  D:\INMAC\${inmacVersion}_powerghost.log -Append
 
-$pdfPath = $pdf+$pdfName  
-#"pdfPath is $pdfPath"| Out-File D:\INMAC\ne.txt -Append 
+# functions #
+
+function ConvertPDF([string]$version, [string]$param1, [string]$pdfPath1)
+{
+    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+    $pinfo.FileName = $version
+    $pinfo.RedirectStandardError = $true
+    $pinfo.RedirectStandardOutput = $true
+    $pinfo.UseShellExecute = $false
+    $pinfo.Arguments = "-q -dNOPAUSE -sDEVICE=tiffg4 $param -r600 $pdfPath -c quit"
+    $p = New-Object System.Diagnostics.Process
+    $p.StartInfo = $pinfo
+    $p.Start() #| Out-Null
+    $timer = $p.WaitForExit(300000);
+}
+
 # MAIN #
 
     Get-Item $pdfPath |   ForEach-Object {   
@@ -39,19 +58,19 @@ $pdfPath = $pdf+$pdfName
         #'Processing ' + $pdf | Out-File -Append "D:\INMAC\logs\${inmacVersion}_ghost.log"
         $workingTif = $outTif -replace '.tif$',''
         $param = "-sOutputFile=$workingTif-%06d.tif"
-
+        #"workingTif is $workingTif"| Out-File  D:\INMAC\${inmacVersion}_powerghost.log -Append
         #Convert the pdf
         $pinfo = New-Object System.Diagnostics.ProcessStartInfo
         $pinfo.FileName = "D:\Program Files\gs\gs9.14\bin\gswin64c.exe"
         $pinfo.RedirectStandardError = $true
         $pinfo.RedirectStandardOutput = $true
         $pinfo.UseShellExecute = $false
-        $pinfo.Arguments = "-q -dNumRenderingThreads=4 -dBandBufferSpace=500000000 -sBandListStorage=memory -dBufferSpace=1000000000 -dNOPAUSE -sDEVICE=tiffg4 $param -r600 -c 30000000 setvmthreshold -f $pdfPath -c quit"
+        $pinfo.Arguments = "-q -dNOPAUSE -sDEVICE=tiffg4 $param -r600 $pdfPath -c quit"
         $p = New-Object System.Diagnostics.Process
         $p.StartInfo = $pinfo
         $p.Start() #| Out-Null
         $timer = $p.WaitForExit(300000);
-
+        
         if (!$timer)
         {
             "$userName - $pdfName timed out @ $(get-date)"| Out-File  D:\INMAC\${inmacVersion}_powerghost.log -Append
@@ -74,9 +93,18 @@ $pdfPath = $pdf+$pdfName
                     "$stderr" | Out-File D:\INMAC\${inmacVersion}_powerghost.log -Append
                     [Environment]::Exit(2)
                 }
+                elseif($stdout.Contains("Error: /undefined in --.PDFexecform--"))
+                {
+                     "------------------------------------------------------------------" | Out-File D:\INMAC\${inmacVersion}_powerghost.log -Append
+                    "$stderr" | Out-File D:\INMAC\${inmacVersion}_powerghost.log -Append
+                    ConvertPDF $repair $param $pdfPath
+                    
+                    #[Environment]::Exit(4)
+                }
                 else 
                 {
                     "------------------------------------------------------------------" | Out-File D:\INMAC\${inmacVersion}_powerghost.log -Append
+                    "$stdout" | Out-File D:\INMAC\${inmacVersion}_powerghost.log -Append
                     "$stderr" | Out-File D:\INMAC\${inmacVersion}_powerghost.log -Append
                     [Environment]::Exit(3)
                 }   
@@ -86,3 +114,4 @@ $pdfPath = $pdf+$pdfName
 
         $error[0] | Out-File D:\INMAC\${inmacVersion}_powerghost.log -Append      
  }
+
