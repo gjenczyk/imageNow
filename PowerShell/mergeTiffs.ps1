7a939188e1007412cb8e3d4c8c76f5c86316997a
gjenczyk <gjenczyk@outlook.com>
2015-04-06 16:48:48 -0400

    Prototype for exporting items

--- PowerShell/mergeTiffs.ps1
+++ PowerShell/mergeTiffs.ps1
@@ -1,13 +1,12 @@
 <# #############################################################################
-# NAME: GenericIntoolRunner.ps1
+# NAME: mergeTiffs.ps1
 # 
 # AUTHOR:  Gregg Jenczyk, UMass (UITS)
-# DATE:  2014/04/18
+# DATE:  2015/04/06
 # EMAIL: gjenczyk@umassp.edu
 # 
-# COMMENT:  This script is a template for running intool scripts via 
-#           PowerShell.  You can use this on demand or to set up scheduled
-#           jobs.  Just be sure to save this as something else.
+# COMMENT:  This script sucks up tiffs put out by an iScript, merges them,
+            converts them to PDF and passes them back to 
 
     INSTRUCTIONS: NEED TO TUNR ON WINRM AND ADD HOSTA TO HOSTB's TRUSTED HOSTS
                   ON EACH SERVER!!!
@@ -39,6 +38,7 @@ $env = $env -replace "W.*",""
 $convServ = "DI${env}WEBRPT01"
 $logDate = $(get-date -format 'yyyyMMdd')
 $scriptName = "mergeTiff" #change this here - no extension
+$returnCode = 0;
 
 #-- WORKING PATHS --#
 $webBaseDir = "D:\inserver6\output\"
@@ -55,25 +55,36 @@ $runLog = "${root}log\run_log-${scriptName}.log"
 #-- MAIN --#
 "$(get-date) - Starting ${scriptName} Script" | Out-File $runLog -Append
 "WEBRPT SERVER = ${convServ}" | Out-File $runLog -Append
-#D:\inserver6\bin64\intool --cmd run-iscript --file ${root}script\${scriptName}.js
 Enter-PSSession -ComputerName $convServ -Authentication NegotiateWithImplicitCredential
-
-New-Item ${mergeDir} -ItemType directory
-
-Get-ChildItem ${webimgDir}* -File | ForEach-Object {
-    "$($_.FullName)" | Out-File $runLog -Append
-    Copy-Item $_ -Destination ${mergeDir}
-    $error[0] | Out-File $runLog -Append
-}
+try {
+    if(-Not $(Test-Path ${mergeDir}))
+    {
+        New-Item ${mergeDir} -ItemType directory
+    }
+    Get-ChildItem ${webimgDir}* -File | ForEach-Object {
+        #"INCOMING TIFF: $($_.FullName)" | Out-File $runLog -Append
+        Copy-Item $_ -Destination ${mergeDir}
+        $error[0] | Out-File $runLog -Append
+    }
 
     $mergeFile = ""
     $pdfOut = ""
     Get-ChildItem ${mergeDir}* -File | ForEach-Object {
-        $renArr = [regex]::split($_.Name,'_|\.')
-        $mergeFile = "${mergeDir}$($renArr[0])_$($renArr[1]).$($renArr[3])"
+        $baseName = $_.BaseName
+        $extension = $_.Extension
+        $renArr = [regex]::split($baseName,'_') 
+        $uniqueId = "$($renArr[0])_$($renArr[1])"
+        $pageNo = $renArr[2]
+        #handle single page exports
+        if($pageNo -eq $null)
+        {
+            $pageNo = "1"
+        }
+        $mergeFile = "${mergeDir}${uniqueId}${extension}"
+        #"MERGE FILE: ${mergeFile}" | Out-File $runLog -Append
         $pdfOut = "${mergeDir}$($renArr[0])_$($renArr[1]).PDF"
-        "$pdfOut" | Out-File $runLog -Append
-        Rename-Item $_ -NewName "$($renArr[2])_$($renArr[0])_$($renArr[1]).$($renArr[3])"
+        #"PDF: $pdfOut" | Out-File $runLog -Append
+        Rename-Item $_ -NewName "${pageNo}_${uniqueId}${extension}"
     }
 
     $tiffArr = @()
@@ -111,31 +122,27 @@ Get-ChildItem ${webimgDir}* -File | ForEach-Object {
     }
 
     & $tiff2pdf $mergeFile -o $pdfOut
-
-    #Remove-Item -Path $mergeFile
-    New-Item -Path ${completeDir} -ItemType directory
+    if(-Not $(Test-Path ${completeDir}))
+    {
+        New-Item -Path ${completeDir} -ItemType directory
+    }
     Move-Item -Path $pdfOut -Destination ${completeDir}
     Remove-Item -Path ${expDir} -Recurse
-Exit-PSSession
-
+}
+catch [system.exception]{
+    $error[0] | Out-File $runLog -Append
+    $returnCode = 1;
+}
+finally
+{
+    Exit-PSSession
+}
 #cleanup
 Get-ChildItem $webimgDir -File | ForEach-Object {
      Remove-Item $_.FullName
 }
 Remove-Item "${webBaseDir}${emplid}\" -Recurse
 
-<#
-Use this if you want to be notified when the script finishes running#>
-
-<#$attachments = @()
-Get-ChildItem -Path ${webimgDir}\complete\* | ForEach-Object {
-$attachments += $_.FullName
-}
-$message = "I guess you want this..."
-sendmail -t "gjenczyk@umassp.edu","cmatera@umassp.edu" -s "[DI ${env} Notice]Here is the pdf you need" -m ${message} -a $attachments
-Get-ChildItem -Path ${webimgDir}\complete\* | ForEach-Object {
-Remove-Item $_.FullName
-}#>
-
 $error[0] | Out-File $runLog -Append
-"$(get-date) - Finishing ${scriptName} Script" | Out-File $runLog -Append
+"$(get-date) - Finishing ${scriptName} Script`n Returned: ${returnCode}" | Out-File $runLog -Append
+[Environment]::Exit($returnCode)
--- iScripts/GA_DocumentExport.js
+++ iScripts/GA_DocumentExport.js
@@ -23,6 +23,10 @@
 #include "$IMAGENOWDIR6$\\script\\lib\\iScriptDebug.jsh"
 #include "$IMAGENOWDIR6$\\script\\lib\\HostDBLookupInfo.jsh"
 #include "$IMAGENOWDIR6$\\script\\STL\\packages\\Document\\exportDocPhsOb.js"
+#include "$IMAGENOWDIR6$\\script\\lib\\yaml_loader.jsh"
+#include "$IMAGENOWDIR6$\\script\\lib\\csvObject.jsh"
+#include "$IMAGENOWDIR6$\\script\\lib\\GetUniqueF5DateTime.js"
+#include "$IMAGENOWDIR6$\\script\\lib\\GetProp.jsh"
 
 // *********************         Configuration        *******************
 
@@ -37,7 +41,6 @@
 
 // ********************* Initialize global variables ********************
 
-sql = "";
 var POWERSHELL_ROOT = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe";
 var POWERSHELL_MERGE = imagenowDir6+"\\script\\PowerShell\\mergeTiffs.ps1"
 
@@ -52,30 +55,106 @@ var POWERSHELL_MERGE = imagenowDir6+"\\script\\PowerShell\\mergeTiffs.ps1"
     {
       debug = new iScriptDebug("GA_DocumentExport", LOG_TO_FILE, DEBUG_LEVEL);
       debug.log("WARNING", "GA_DocumentExport script started.\n");
+
+      //get informaiton about the current wf item
       var wfItem = new INWfItem("301YY4P_04VZ7RN4N0142Y5");//currentWfItem.id);//
       if(!wfItem.id || !wfItem.getInfo())
       {
         debug.log("CRITICAL", " Couldn't get info for wfItem: %s\n", getErrMsg());
         return false;
       }
+
+      //get queue name
+      var queueName = wfItem.queueName;
+
+      //store the results from the yaml here
+      var docTypeList = "";
+      var pdfDocType = "";
+      var pdfDocQCfg = imagenowDir6 + "\\script\\";
+
       //collect information about who routed the document into the queue
-      printf(wfItem.queueStartUserName);
       var router = new INUser(wfItem.queueStartUserName);
-       //get information about inbound doc
-       //determine docs to send
-       //convert matching docs to pdfs
-       //email to person who routed doc in
-       //forward doc to complete queue
-       //get doc
-    	var doc = new INDocument(wfItem.objectId);//"301YY4P_04VZ7RN4N0142XW");  
-    	if(!doc.getInfo())
-    	{
-    	    printf("Couldn't get doc info: %s.\n",
-    	    getErrMsg()); return false;
-    	}
+      if(!router.getInfo())
+      {
+        debug.log("ERROR","Could not get user information for [%s] - [%s]\n",wfItem.queueStartUserName,getErrMsg());
+        return false;
+      }
+      //make sure user has an email address on file
+/*      if(!router.email || router.email == null)
+      {
+        debug.log("ERROR","No email address found for [%s - %s %s]\n",router.id, router.lastName, router.firstName);
+        return false;
+      }
+*/
+      //get information about inbound doc
+      var doc = new INDocument(wfItem.objectId);//"301YY4P_04VZ7RN4N0142XW");  
+      if(!doc.getInfo())
+      {
+          debug.log("ERROR","Couldn't get doc info: [%s]\n",getErrMsg()); 
+          return false;
+      }
+      //get doc keys
+      var drawer = doc.drawer;
+      var emplid = doc.field1;
+      var name = doc.field2;
+      var term = doc.field3;
+      var plan = doc.field4;
 
-      //exportDoc(doc);
+      //identify the correct doctype list to use for exporting in the yaml
+      loadYAMLConfig(imagenowDir6+"\\script\\config_scripts\\GA_DocumentExport\\");
+      for (var office in CFG.GA_DocumentExport)
+      {
+        var exportConfig = CFG.GA_DocumentExport[office].EXPORT_CONFIG;
+        for (var i = 0; i < exportConfig.length ; i++)
+        {
+          if (exportConfig[i].SCRIPT_Q == queueName)
+          {
+            docTypeList = exportConfig[i].DOCTYPE_LIST;
+            pdfDocType = exportConfig[i].IN_PDF_DOC;
+            pdfDocQCfg += exportConfig[i].CREATE_IN;
+            break;
+          }
+        }
+      }//end checking the yaml for the list
 
+      //get out if we can't find a list in the config
+      if((!docTypeList || docTypeList == null) || (!pdfDocType || pdfDocType == null) || (!pdfDocQCfg || pdfDocQCfg == null))
+      {
+        debug.log("ERROR","Invalid config for [%s] - docTypeList:[%s] pdfDocType:[%s] pdfDocQCfg:[%s]\n",queueName,docTypeList,pdfDocType,pdfDocQCfg);
+        return false;
+      }
+
+      //get a list of documents to send
+      var docList = findDocsToSend(emplid, drawer, docTypeList);
+
+      if (!docList || docList == null)
+      {
+        debug.log("ERROR","Couln't find exportable documents for [%s] in [%s] and [%s]\n",emplid, drawer, docTypeList);
+        return false;
+      }
+
+      //prepare the documents for emailing
+/*      while(docList.next())
+      {
+        var tiffDoc = new INDocument(docList[0]);
+        if(!tiffDoc.getInfo())
+        {
+            debug.log("ERROR","Couldn't get doc info: [%s]\n",getErrMsg()); 
+            //what to do here? return false;
+        }
+
+        if(!exportDoc(tiffDoc))
+        {
+          debug.log("ERROR","Could not export [%s]\n",tiffDoc.id);
+          //what to do here?
+        }
+      }//end of the exporting
+*/
+      var revDoc = createNewDoc(drawer,emplid,name,term,plan,pdfDocType);
+
+
+      //prpbably should have some kind of check here...
+      addDocToQueue(revDoc, pdfDocQCfg);
     }
         
     catch(e)
@@ -114,6 +193,35 @@ var POWERSHELL_MERGE = imagenowDir6+"\\script\\PowerShell\\mergeTiffs.ps1"
 }
 
 // ********************* Function Definitions **********************************
+function findDocsToSend(emplid, drawer, list)
+{
+  sql = "SELECT INUSER.IN_DOC.DOC_ID, " +
+        "INUSER.IN_DOC_TYPE.DOC_TYPE_NAME " +
+        "FROM INUSER.IN_DOC " +
+        "INNER JOIN INUSER.IN_DOC_TYPE " +
+        "ON INUSER.IN_DOC_TYPE.DOC_TYPE_ID = INUSER.IN_DOC.DOC_TYPE_ID " +
+        "INNER JOIN INUSER.IN_DOC_TYPE_LIST_MEMBER " +
+        "ON INUSER.IN_DOC_TYPE.DOC_TYPE_ID = INUSER.IN_DOC_TYPE_LIST_MEMBER.DOC_TYPE_ID " +
+        "INNER JOIN INUSER.IN_DOC_TYPE_LIST " +
+        "ON INUSER.IN_DOC_TYPE_LIST.DOC_TYPE_LIST_ID = INUSER.IN_DOC_TYPE_LIST_MEMBER.DOC_TYPE_LIST_ID " +
+        "INNER JOIN INUSER.IN_DRAWER " +
+        "ON INUSER.IN_DRAWER.DRAWER_ID = INUSER.IN_DOC.DRAWER_ID " +
+        "WHERE INUSER.IN_DOC.FOLDER    = '" + emplid + "' " +
+        "AND INUSER.IN_DRAWER.DRAWER_NAME like '" + drawer + "%' " +
+        "AND LIST_NAME = '" + list + "';"
+
+        var returnVal;
+        var cur = getHostDBLookupInfo_cur(sql,returnVal);
+
+        if(!cur || cur == null)
+        {
+          debug.log("WARNING","no results returned for query.\n");
+          return false;
+        }
+
+        return cur; 
+}
+
 function exec(cmd, expected_return)
 {
   debug.log("INFO", "Exec cmd: *%s*\n", cmd);
@@ -139,21 +247,115 @@ function exportDoc(doc)
   Clib.mkdir(exportDir);
   if(!exportDocPhsOb(doc,exportDir + "\\"+doc.id+"\\","ALL","ALL",true))
   {
-    printf(getErrMsg());
+    debug.log("ERROR","Could not export [%s] - [%s]\n",doc.id,getErrMsg());
+    return false;
   }
   else
   {
     var cmd = "";
     Clib.sprintf(cmd, '%s %s %s %s', POWERSHELL_ROOT, POWERSHELL_MERGE, doc.field1, doc.id);
     var rtn = exec(cmd, 0);
+    return rtn;
+  }
+}//end export doc
+
+function createNewDoc(drawer,field1,field2,field3,field4,type)
+{
+  //get unique dateTime
+  var linkDate_obj = new GetUniqueF5DateTime(true);
+  var linkDate = linkDate_obj.tellTheTime();
+
+  var keys = new INKeys(drawer,field1,field2,field3,field4,linkDate,type);
+  debug.log("INFO","Creating new document: [%s]\n",keys.toString());
+  var pdfDoc = new INDocument(keys);
+return pdfDoc;
+  //or use doc Id to create a document instance.
+  var filePath = "D:\\inserver6\\output\\complete\\" + field1 + "\\";
+  var pdfImports = SElib.directory(filePath + "*");
+  for (var j = 0; j < pdfImports.length; j++)
+  {
+    debug.log("INFO","pdfImports[%s] = [%s]\n",j, pdfImports[j].name);
+    var parts = SElib.splitFilename(pdfImports[j].name);
+    var workingPdf = parts.name + parts.ext;
+    var attr = new Array;
+    attr["phsob.file.type"] = parts.ext;
+    attr["phsob.working.name"] = workingPdf;
+    attr["phsob.source"]="GA_DocumentExport";
+    var logob = pdfDoc.storeObject(pdfImports[j].name, attr);
+    if (logob == null)
+    {
+        debug.log("ERROR","Could not import document:%s.\n", getErrMsg());
+        return false;
+    }
+    else
+    {
+      debug.log("INFO","Successfully imported logobID: %s.\n", logob.id);
+      if(!Clib.remove(pdfImports[j].name))
+      {
+        debug.log("ERROR","Could not delete: [%s] - [%s]\n",pdfImports[j].name, getErrMsg());
+      }
+      else
+      {
+        debug.log("INFO","Successfully deleted [%s]\n",pdfImports[j].name);
+      }
+    }
+  }//end of for each pdf
+  if(!Clib.rmdir(filePath))
+  {
+    debug.log("ERROR","Could not remove [%s] - [%s]\n",filePath, getErrMsg());
+  }
+  else
+  {
+    debug.log("INFO","Removed [%s]\n",filePath);
+  }
+
+  return pdfDoc;
+
+}//end create new doc
+
+function addDocToQueue(doc, routeCfg)
+{
+  doc.getInfo();
+  var docPlan = doc.field4;
+  var subPlan = GetProp(doc,"Sub-Plan",false);
+
+  var columnConfig =
+  [
+    {name:'plan'},
+    {name:'subplan'},
+    {name:'subqueue'}
+  ];
+  var objCSV = new csvObject(routeCfg, columnConfig, {intHeaderLen:1, delim:',', innerDelim:''});
+  if(!objCSV.openFile('r'))
+  {
+    debug.log("ERROR", "loadPlanSubPlanQueues: unable to open file at [%s]\n", routeCfg);
+    return false;
+  }
+
+  var rtnObject = {};
+  var row =0;
+  while(true)
+  {
+    row++;
+    var line = objCSV.getNextRowObject();
+    if(line === false)
+    {
+      debug.log("CRITICAL", "loadPlanSubPlanQueues: unable to read data on row [%s]\n", row);
+      break;
+    }
+    else if(line === null)
+    {
+      //debug.log("DEBUG", "loadPlanSubPlanQueues: end of file\n");
+      break;
+    }
+
+    //if (docPlan === line['plan'])
+    //{
+      printf("%s %s\n",line['subplan'],line['plan']);
+    //}
 
   }
+  objCSV.closeFile();
 }
-
-function emailDocs()
-{
-
-}
-
 
 //
